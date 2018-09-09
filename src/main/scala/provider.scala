f2fc6c6d5fe9eae69fd559ad1881c8f2195cfe7a
Dakratus <developer.v.s.k@gmail.com>
2018-09-09 20:00:37 +0300

    added fcm provider

--- src/main/scala/provider.scala
+++ src/main/scala/provider.scala
@@ -5,6 +5,8 @@ import pureconfig.loadConfig
 
 
 class DefaultFcmConfigProvider @Inject()(configuration: Config) extends FcmConfigProvider {
-  
+  lazy val config: FcmConfig = loadConfig[FcmConfig](configuration, "fcm") match {
+    case Left(failures) ⇒ throw new IllegalStateException(s"FCM configuration error(s): ${failures.toList.map(_.description).mkString(", ")}")
+    case Right(c) ⇒ c
   }
 }
